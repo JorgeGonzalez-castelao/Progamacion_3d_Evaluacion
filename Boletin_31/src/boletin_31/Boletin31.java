6422e2277ef2ecde8ed32681d127e4139631f905
Jorge Gonzalez Ruiz <113522749+JorgeGonzalez-castelao@users.noreply.github.com>
2023-05-16 00:43:55 +0200

    Add files via upload
    Boletin_31

---
+++ Boletin_31/src/boletin_31/Boletin31.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boletin_31;
+
+/**
+ *
+ * @author Jorge_Gonzalez
+ */
+public class Boletin31 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaCalculadora obx = new VentanaCalculadora();
+        obx.setVisible(true);
+    }
+    
+}
